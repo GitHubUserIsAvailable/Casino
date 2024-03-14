132ffa288d54a675690d6990b44037be156371bd
borngraeberm <sabbimiti@gmail.com>
2024-03-14 15:20:56 +0100

    Game logic anfänge (game loop) split und double jetzt grob funktionsfähig.

--- src/blackjack/Blackjack.java
+++ src/blackjack/Blackjack.java
@@ -12,6 +12,7 @@ public class Blackjack {
     private static final Scanner scanner = new Scanner(System.in);
     public static Dealer dealer = new Dealer();
     private static String command;
+    private static int handValue;
 
     public static void main(String[] args) {
         System.out.println("----------------------------------------------");
@@ -41,15 +42,13 @@ public class Blackjack {
                 System.out.println("----------------------------------------------");
                 System.out.println("Folgende Befehle sind verfügbar: take card, stand, double, split");
                 System.out.println("Gebe deinen Befehl ein: ");
-                command = scanner.nextLine(); //GAME LOOP SHOULD START HERE -----------------------------------------
-                player.checkCommand(player.getPlayerCardsHand1());
-                if (command.equals("split")) {
-                    splitGame(player, player.getPlayerCardsHand1());
+                command = scanner.nextLine();
+
+                boolean checkedCommand = player.checkCommand(player.getPlayerCardsHand1(), player.getPlayerCardsHand2());
+                if (checkedCommand && command.equals("split")) {
+                    splitGame(player, player.getPlayerCardsHand1(), player.getPlayerCardsHand2());
                 } else {
-                    System.out.println("Cards Dealer: " + List.of(dealer.getDealerCards().get(0).toString(false) + dealer.getDealerCards().get(1).toString(true)));
-                    System.out.println("Gesetzt: " + dealer.getCollectedAmount());
-                    System.out.println("Verfügbares Geld: " + player.getAmount());
-                    System.out.println("----------------------------------------------");
+                    gameLoop(player);
                 }
 
             } else {
@@ -57,27 +56,52 @@ public class Blackjack {
                 System.out.println("Verfügbares Geld: " + player.getAmount());
                 System.out.println("Gebe deinen Wetteinsatz ein: ");
                 input = Integer.parseInt(scanner.nextLine());
-
-
             }
         }
     }
 
-    private static void splitGame(Player player, List<Card> hand) {
+    private static void gameLoop(Player player) {
+        for (int i = 0; i < 20; i++) {
+            handValue = player.checkHandValue(player.getPlayerCardsHand1());
+            System.out.println("----------------------------------------------");
+            System.out.println("Cards " + player.getName() + " " + player.getPlayerCardsHand1().stream().map(card -> card.toString(false)).toList());
+            System.out.println("Cards Dealer: " + List.of(dealer.getDealerCards().get(0).toString(false) + dealer.getDealerCards().get(1).toString(true)));
+            System.out.println("Gesetzt: " + dealer.getCollectedAmount());
+            System.out.println("Verfügbares Geld: " + player.getAmount());
+            System.out.println("Deine Karten haben einen Wert in höhe von: " + handValue);
+            System.out.println("Gebe deinen Befehl ein: ");
+            System.out.println("Folgende Befehle sind verfügbar: take card, stand");
+            command = scanner.nextLine();
+            player.checkCommand(player.getPlayerCardsHand1(), player.getPlayerCardsHand2());
+        }
+
+
+    }
+
+    private static void splitGame(Player player, List<Card> hand, List<Card> hand2) {
         List<List<Card>> hands = new ArrayList<>();
         hands.add(player.getPlayerCardsHand1());
         hands.add(player.getPlayerCardsHand2());
         hands.add(player.getPlayerCardsHand3());
         hands.add(player.getPlayerCardsHand4());
+        int displayHandNumber;
 
-        for (int i = 1; i < 10; i++) {
-            List<Card> currentHand = hands.get(i % hands.size());
-            System.out.println("Cards " + player.getName() + " " + i  + " " + currentHand.stream().map(card -> card.toString(false)).toList());
-            System.out.println("(take card, stand, double, split)");
-            System.out.println("Gebe deinen Befehl ein: ");
-            command = scanner.nextLine();
-            player.checkCommand(currentHand);
-
+        for (int i = 0; i < 10; i++) {
+            if (i >= 4) {
+                i = 0;
+            }
+            displayHandNumber = i;
+            List<Card> currentHand = hands.get(i);
+            System.out.println("SPLIT METHODE AUSGERUFEN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            if (!hands.get(i).isEmpty()) {
+                System.out.println("Cards " + player.getName() + " " + (displayHandNumber + 1) + " " + currentHand.stream().map(card -> card.toString(false)).toList());
+                System.out.println("(take card, stand, double, split)");
+                System.out.println("Gebe deinen Befehl ein: ");
+                command = scanner.nextLine();
+                player.checkCommand(currentHand, hand2);
+            } else {
+                i = -1;
+            }
         }
     }
 
@@ -85,6 +109,12 @@ public class Blackjack {
         return command;
     }
 
+
+    public static void setCommand(String command) {
+        Blackjack.command = command;
+    }
 }
+
+
 
 
